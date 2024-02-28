6ee4240d679e9af44d3876699681a15643658c30
pfigithub <pampam7072@gmail.com>
2024-02-28 13:51:19 +0330

    select and separation

--- K-Nearest-Neighbors.py
+++ K-Nearest-Neighbors.py
@@ -7,3 +7,7 @@ from sklearn import preprocessing
 # reading data
 df = pd.read_csv('teleCust1000t.csv')
 df.head()
+
+# selecting features for x and y(base on our data)
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+y = df['custcat'].values

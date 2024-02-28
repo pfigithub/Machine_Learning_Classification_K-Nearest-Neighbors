b020697054ff7ab108649c076637094ac2c576e4
pfigithub <pampam7072@gmail.com>
2024-02-28 13:54:00 +0330

    split

--- K-Nearest-Neighbors.py
+++ K-Nearest-Neighbors.py
@@ -15,3 +15,9 @@ y = df['custcat'].values
 # normalizing data
 X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
 X[0:5]
+
+# train-test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
